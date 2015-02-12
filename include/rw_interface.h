380dde8fe73ce73ed0821b31b8a68e918ac511f8
drdrpyan <drdrpyan@gmail.com>
2015-02-12 22:32:45 +0900

    들여쓰기 수정

--- include/rw_interface.h
+++ include/rw_interface.h
@@ -13,13 +13,13 @@ namespace IO
 {
 
 class RWInterface {
-  public:
-	virtual ~RWInterface() {}
+ public:
+  virtual ~RWInterface() {}
 
-    virtual int read(std::istream stream) = 0;  //스트림에서 객체를 읽어들인다. 읽어들인 바이트를 반환
-    virtual int read(FILE *stream) = 0;
-    virtual int write(std::ostream stream) = 0; //스트림에 객체를 쓴다. 쓴 바이트를 반환
-    virtual int write(FILE *stream) = 0;
+  virtual int read(std::istream stream) = 0;  //스트림에서 객체를 읽어들인다. 읽어들인 바이트를 반환
+  virtual int read(FILE *stream) = 0;
+  virtual int write(std::ostream stream) = 0; //스트림에 객체를 쓴다. 쓴 바이트를 반환
+  virtual int write(FILE *stream) = 0;
 };
 
 }
