8d4fa6faba7a09d3f745cecf8e734b0d1a5ad36b
drdrpyan <drdrpyan@gmail.com>
2015-02-13 00:05:53 +0900

    가상 소멸자 추가

--- include/tree_node.h
+++ include/tree_node.h
@@ -10,6 +10,7 @@ template<class T>
 class TreeNode : public Wrapper<T>
 {
  public:
+  virtual ~TreeNode();
   virtual const TreeNode<T>* child(unsigned int idx)=0;
   virtual TreeNode<T>* child(unsigned int idx)=0;
   virtual void set_child(unsigned int idx, const TreeNode<T> *child)=0;
