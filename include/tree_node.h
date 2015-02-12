4fbaed019a362ed8fd7de9c625e1eff13611d8f4
drdrpyan <drdrpyan@gmail.com>
2015-02-12 23:23:29 +0900

    tree node 초안 작성

---
+++ include/tree_node.h
@@ -0,0 +1,18 @@
+#ifndef BGM_DATA_TREENODE_H
+#define BGM_DATA_TREENODE_H
+
+namespace BGM
+{
+namespace DATA
+{
+
+template<class T>
+class TreeNode : public Wrapper<T>
+{
+ public:
+	//함수 나중에 포함할 것
+};
+
+}
+}
+#endif
