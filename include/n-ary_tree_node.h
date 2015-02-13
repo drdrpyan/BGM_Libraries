cb27fab39825650f6c28474d6807c218dba82ee9
drdrpyan <drdrpyan@gmail.com>
2015-02-13 17:31:47 +0900

    들여쓰기 수정

--- include/n-ary_tree_node.h
+++ include/n-ary_tree_node.h
@@ -14,15 +14,15 @@ class NAryTreeNode : public TreeNode<T>
     delete[] child_;
   }
   NAryTreeNode(const T *data) : Wrapper(data) {
-	  memset(child_, static_cast<int>(nullptr), num_of_child);
+    memset(child_, static_cast<int>(nullptr), num_of_child);
   }
   virtual const NAryTreeNode<T, num_of_child>* child(unsigned int idx) {
-	  assert(idx >= num_of_child);
-	  return child_[idx];
+    assert(idx >= num_of_child);
+    return child_[idx];
   }
   virtual NAryTreeNode<T, num_of_child> child(unsigned int idx) {
     return const_cast<T&>(static_cast<const NAryTreeNode<T,
-			                num_of_child>*>(this)->child(idx));
+			              num_of_child>*>(this)->child(idx));
   }
   virtual void set_child(unsigned int idx,
 		                 const NAryTreeNode<T, num_of_child> *child) {
