936335e39801cd93d555cc5a43513a3d0f64d6be
drdrpyan <drdrpyan@gmail.com>
2015-02-13 18:46:52 +0900

    초안 작성

--- include/binary_tree_node.h
+++ include/binary_tree_node.h
@@ -9,7 +9,29 @@ namespace DATA
 template <class T>
 class BinaryTreeNode : public NAryTreeNode<T, 2>
 {
+  public:
+	explicit BinaryTreeNode(const T &data) : NAryTreeNode<T, 2>(data) { }
+	virtual ~BinaryTreeNode() { }
+    virtual const BinaryTreeNode* left_child() const {
+      return static_cast<const BinaryTreeNode*>(child(0));
+    }
+    virtual BinaryTreeNode* left_child() const {
+      return const_cast<BinaryTreeNode*>(
+                 static_cast<const BinaryTreeNode*>(this)->left_child());
+    }
+    virtual const BinaryTreeNode* right_child() const {
+      return static_cast<const BinaryTreeNode*>(child(1));
+    }
+    virtual BinaryTreeNode* rightChild() const {
+      return const_cast<BinaryTreeNode*>(
+                 static_cast<const BinaryTreeNode*>(this)->right_child());
+    }
+    virtual void set_left_child(const BinaryTreeNode *left_child) {
 
+    }
+    virtual void set_right_child(const BinaryTreeNode *right_child) {
+
+    }
 };
 
 }
