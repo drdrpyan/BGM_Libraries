6920157093eaa20478e089425fe48350d2a564a0
drdrpyan <drdrpyan@gmail.com>
2015-03-04 21:58:24 +0900

    TreeNode의 생성자, 접근자 수정

--- include/tree_node.h
+++ include/tree_node.h
@@ -12,11 +12,13 @@ template<class T>
 class TreeNode : public Wrapper<T>
 {
  public:
-  explicit TreeNode(const T &data) : Wrapper(data) { }
+  explicit TreeNode(const T &data) : Wrapper<T>(data) { }
   virtual ~TreeNode() { }
   virtual const TreeNode<T>* child(unsigned int idx) const = 0;
-  virtual TreeNode<T>* child(unsigned int idx) const = 0;
-  virtual void set_child(unsigned int idx, const TreeNode<T> *child)=0;
+  virtual TreeNode<T>* child(unsigned int idx) {
+    return const_cast<TreeNode<T>*>(static_cast<const TreeNode<T>*>(this)->child(idx));
+  }
+  virtual void set_child(unsigned int idx, const void *child_node) = 0;
 };
 
 }
