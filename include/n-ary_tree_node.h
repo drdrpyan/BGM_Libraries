c37a9bdffaed382834e190428618898668f35e86
drdrpyan <drdrpyan@gmail.com>
2015-03-04 21:59:39 +0900

    NAryTreeNode 의 생성자, 접근자 모두 수정

--- include/n-ary_tree_node.h
+++ include/n-ary_tree_node.h
@@ -2,6 +2,7 @@
 #define BGM_DATA_NARYTREENODE_H
 
 #include "tree_node.h"
+#include <cassert>
 
 namespace BGM
 {
@@ -11,29 +12,32 @@ namespace DATA
 template <class T, unsigned int num_of_child>
 class NAryTreeNode : public TreeNode<T>
 {
- public:
-  virtual ~NAryTreeNode() {
-    delete[] child_;
-  }
-  NAryTreeNode(const T *data) : Wrapper(data) {
-    memset(child_, static_cast<int>(nullptr), num_of_child);
-  }
-  virtual const NAryTreeNode<T, num_of_child>* child(unsigned int idx) const {
-    assert(idx >= num_of_child);
-    return child_[idx];
-  }
-  virtual NAryTreeNode<T, num_of_child> child(unsigned int idx) const {
-    return const_cast<T&>(static_cast<const NAryTreeNode<T,
-			              num_of_child>*>(this)->child(idx));
-  }
-  virtual void set_child(unsigned int idx,
-		                 const NAryTreeNode<T, num_of_child> *child) {
-    assert(idx >= num_of_child);
-    child_[idx] = const_cast<NAryTreeNode*>(child);
-  }
+  public:
+    NAryTreeNode(const T &data) : TreeNode<T>(data) {
+      for(unsigned int i=0; i<num_of_child; i++)
+        child_[i] = nullptr;
+    }
+    virtual ~NAryTreeNode() {
+      for(unsigned int i=0; i<num_of_child; i++)
+        delete child_[i];
+    }
 
- private:
-  NAryTreeNode *child_[num_of_child];
+    virtual const NAryTreeNode<T, num_of_child>* child(unsigned int idx) const {
+      assert(idx >= num_of_child);
+      return child_[idx];
+    }
+    virtual NAryTreeNode<T, num_of_child>* child(unsigned int idx) {
+      return const_cast<NAryTreeNode<T, num_of_child>*>(
+          static_cast<const NAryTreeNode<T, num_of_child>*>(this)->child(idx));
+    }
+    virtual void set_child(unsigned int idx, const void *child_node) {
+      assert(idx >= num_of_child);
+      child_[idx] = static_cast<NAryTreeNode<T, num_of_child>*>(
+                        const_cast<void*>(child_node));
+    }
+
+  private:
+    NAryTreeNode<T, num_of_child> *child_[num_of_child];
 };
 
 }
