cbb1121386be1fd3e8936c31856db5c9dd2e181a
drdrpyan <drdrpyan@gmail.com>
2015-03-04 23:01:52 +0900

    BinaryTreeNode 전체 수정

--- include/binary_tree_node.h
+++ include/binary_tree_node.h
@@ -14,25 +14,27 @@ class BinaryTreeNode : public NAryTreeNode<T, 2>
   public:
 	explicit BinaryTreeNode(const T &data) : NAryTreeNode<T, 2>(data) { }
 	virtual ~BinaryTreeNode() { }
-    virtual const BinaryTreeNode* left_child() const {
-      return static_cast<const BinaryTreeNode*>(child(0));
+    virtual const BinaryTreeNode<T>* left_child() const {
+      return static_cast<const BinaryTreeNode<T>*>(
+                 NAryTreeNode<T, 2>::child(0));
     }
-    virtual BinaryTreeNode* left_child() const {
-      return const_cast<BinaryTreeNode*>(
-                 static_cast<const BinaryTreeNode*>(this)->left_child());
+    virtual BinaryTreeNode<T>* left_child() {
+      return const_cast<BinaryTreeNode<T>*>(
+                 static_cast<const BinaryTreeNode<T>*>(this)->left_child());
     }
-    virtual const BinaryTreeNode* right_child() const {
-      return static_cast<const BinaryTreeNode*>(child(1));
+    virtual const BinaryTreeNode<T>* right_child() const {
+      return static_cast<const BinaryTreeNode<T>*>(
+                 NAryTreeNode<T, 2>::child(1));
     }
-    virtual BinaryTreeNode* rightChild() const {
-      return const_cast<BinaryTreeNode*>(
-                 static_cast<const BinaryTreeNode*>(this)->right_child());
+    virtual BinaryTreeNode<T>* rightChild() {
+      return const_cast<BinaryTreeNode<T>*>(
+                 static_cast<const BinaryTreeNode<T>*>(this)->right_child());
     }
-    virtual void set_left_child(const BinaryTreeNode *left_child) {
-      set_child(0, left_child);
+    virtual void set_left_child(const BinaryTreeNode<T> *left_child) {
+      NAryTreeNode<T, 2>::set_child(0, left_child);
     }
-    virtual void set_right_child(const BinaryTreeNode *right_child) {
-      set_child(1, right_child);
+    virtual void set_right_child(const BinaryTreeNode<T> *right_child) {
+      NAryTreeNode<T, 2>::set_child(1, right_child);
     }
 };
 
